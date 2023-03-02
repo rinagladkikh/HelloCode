13d93a4037ec6f8fa7002ffb04303b83d8639882
rina_gladkikh <rina.gladkikh@gmail.com>
2023-03-03 00:59:25 +0300

    Добавили ToLower

---
+++ Example005_Condition_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ирина")
+{
+    Console.WriteLine("Ура, это же ИРИНА!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
